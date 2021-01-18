af12be7a16efb98b399e6377641882c40d2cbef6
muladiaprianto <62415538+muladiaprianto@users.noreply.github.com>
2021-01-18 19:30:45 +0700

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ class Hello{
         System.out.println("Judul");
         System.out.println("Moelogy: Aplikasi Java 2020");
         System.out.println("--- *** ---");
+        System.out.println("END");
     }
 } 
