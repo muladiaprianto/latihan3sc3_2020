037bbbf514edd5bb100a3ee6e28fdfa14bcf717d
Moelogy <apriantomuladi@gmail.com>
2021-01-18 19:27:26 +0700

    Third Commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello{
     public static void main(String[] args){
+        System.out.println("Judul");
         System.out.println("Moelogy: Aplikasi Java 2020");
+        System.out.println("--- *** ---");
     }
 } 
