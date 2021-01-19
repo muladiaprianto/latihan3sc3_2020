0730902124f1c68b7da6234da3b689b8d2bded82
putri <putrinatalia96@gmail.com>
2021-01-19 20:48:59 +0700

    Sixth Commit

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ class Hello{
         System.out.println("Moelogy: Aplikasi Java 2020");
         System.out.println("--- *** ---");
         System.out.println("END");
-	System.out.println("User 2");
+	System.out.println("Another User");
     }
 } 
